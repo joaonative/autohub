c9865a03c021411092cbdedd66bebb5621260490
joaonative <jestudos127@gmail.com>
2024-05-20 12:13:24 -0300

    tailwind impl

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#FFA31A",
+                gray: "#C7C7C7",
+                darker: "#121212",
+                dark: "#1B1B1B",
+            },
+        },
+    },
+    plugins: [],
+};
