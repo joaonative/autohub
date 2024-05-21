6cd4071ccf5c1dd67b58b0d4796ecbba911700ec
joaonative <jestudos127@gmail.com>
2024-05-20 22:28:44 -0300

    new tables & user migration

---
+++ database/migrations/2024_05_20_221947_edit_user_info.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('adm')->default(false);
+            $table->string('imageUrl')->default('/default.webp');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('adm');
+            $table->dropColumn('imageUrl');
+        });
+    }
+};
